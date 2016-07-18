c562c241e20fd5bd58e5db34e63c6b2c5aed2273
Arthur <fortes.arthur@gmail.com>
2016-07-18 12:49:45 -0300

    Create a pypi library

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
 __author__ = "Arthur Fortes"
 
 setup(
     name='CaseRecommender',
-    packages=['CaseRecommender'],
+    packages=find_packages(),
     version='0.0.4',
     license='GPL3 License',
     description='A recommender systems CaseRecommender for python',
@@ -13,5 +15,6 @@ setup(
     url='https://github.com/ArthurFortes/CaseRecommender',
     download_url='https://github.com/ArthurFortes/CaseRecommender/tarball/0.0.3',
     keywords=['recommender systems', 'CaseRecommender', 'collaborative filtering', 'content-based filtering'],
+    install_requires=["scipy", "numpy"],
     classifiers=[],
 )
