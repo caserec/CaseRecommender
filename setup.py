fd9380f1a0cf26f4050e7a2cec033de81cf9cc56
Arthur Fortes <fortes.arthur@gmail.com>
2017-05-30 12:26:14 -0300

    - New Version to pypi

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ __author__ = "Arthur Fortes"
 setup(
     name='CaseRecommender',
     packages=find_packages(),
-    version='0.0.16',
+    version='0.0.17',
     license='GPL3 License',
     description='A recommender systems framework for Python',
     author='Arthur Fortes',
     author_email='fortes.arthur@gmail.com',
     url='https://github.com/ArthurFortes/CaseRecommender',
-    download_url='https://github.com/ArthurFortes/CaseRecommender/tarball/0.0.16',
+    download_url='https://github.com/ArthurFortes/CaseRecommender/tarball/0.0.17',
     keywords=['recommender systems', 'framework', 'collaborative filtering', 'content-based filtering'],
     classifiers=[],
 )
