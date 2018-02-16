2c34a0080493e5c7b8a5b609dffb4638332304ab
Arthur Fortes <fortes.arthur@gmail.com>
2018-02-16 21:38:32 -0200

    Upload to pypi

--- setup.py
+++ setup.py
@@ -7,15 +7,23 @@
 
 from distutils.core import setup
 from setuptools import find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
 
 __author__ = 'Arthur Fortes <fortes.arthur@gmail.com>'
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='CaseRecommender',
     packages=find_packages(),
-    version='1.0.0',
+    version='1.0.1',
     license='MIT License',
     description='A recommender systems framework for Python',
+    long_description=long_description,
 
     author='Arthur Fortes',
     author_email='fortes.arthur@gmail.com',
