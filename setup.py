2e5033fb980fc9d5d02502a635f504f8fb7dc755
Arthur Fortes da Costa <costaaf@motorola.com>
2019-08-20 10:37:04 -0300

    Update new version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ REQUIRED_PACKAGES = [
 setup(
     name='CaseRecommender',
     packages=find_packages(),
-    version='1.0.1001_1',
+    version='1.1.0',
     license='MIT License',
     description='A recommender systems framework for Python',
     long_description=long_description,
